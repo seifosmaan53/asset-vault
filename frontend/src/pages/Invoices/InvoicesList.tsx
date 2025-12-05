f2994ff6ea193e71de3eb258f2a98cea2a9beb4f
seif osman <54346389+seifosmaan53@users.noreply.github.com>
2025-12-05 13:59:56 -0500

    chore: add Skeleton import for loading states

--- frontend/src/pages/Invoices/InvoicesList.tsx
+++ frontend/src/pages/Invoices/InvoicesList.tsx
@@ -19,6 +19,7 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
+  Skeleton,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
